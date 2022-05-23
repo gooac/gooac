ffdb07505a136091001daedd489685c941a3eb2f
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2022-05-22 23:50:00 -0700

    feat: tokenizer

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/garryspins/gooa/v1
+
+go 1.18
---
+++ tokenizer_test.go
@@ -0,0 +1,27 @@
+package gooa
+
+import (
+	// "fmt"
+	"testing"
+)
+
+func TestTokenizer(*testing.T) {
+// 	tk := &Tokenizer{}
+
+// 	goto_tokens := tk.Tokenize(`
+// function a(a) 
+
+// end
+// 	`)
+	// for k, v := range goto_tokens {
+		// fmt.Printf("GOTO:\t%v - ", k)
+		// v.Print()
+	// }
+
+	// println()
+	// print_tokens := tk.Tokenize("print('balls  '  )  ") 
+	// for k, v := range print_tokens {
+	// 	fmt.Printf("PRINT:\t%v - ", k)
+	// 	v.Print()
+	// }
+}

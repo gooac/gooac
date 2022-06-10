bec1cb62e4414db57ac923ef5057f749a52b6ab3
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2022-06-10 02:46:57 -0700

    another test change

--- tests/test1.lua
+++ tests/test1.lua
@@ -3,3 +3,35 @@
 print(123)
 
 print("some test change")
+
+function foo(arguments)
+    function foo(arguments)
+        function foo(arguments)
+            function foo(arguments)
+                function foo(arguments)
+                    function foo(arguments)
+                        function foo(arguments)
+                            function foo(arguments)
+                                function foo(arguments)
+                                    function foo(arguments)
+                                        function foo(arguments)
+                                            function foo(arguments)
+                                                function foo(arguments)
+                                                    function foo(arguments)
+                                                        function foo(arguments)
+                                            
+                                                        end
+                                                    end
+                                                end
+                                            end
+                                        end
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
